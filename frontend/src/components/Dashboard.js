5d5e02df0dacdeb40e6535a152dc4cc3c9f86c5d
Samuel Agbedia <solumide83@gmail.com>
2024-08-23 18:51:49 -0700

    Dashboard redesign done

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import '../Dashboard.css';
-import SettingsIcon from '../assets/images/SettingsIcon.png';
+// import SettingsIcon from '../assets/images/SettingsIcon.png';
+// import SearchIcon from '../assets/images/SearchIcon.png';
 
 const DashboardPage = () => {
     return (
-        <div className="dashboard-container">
+        <div>
+            <div>
             <header className="header">
                 <h1 className="logo">CHARGETRAILS</h1>
-                <img src={SettingsIcon} alt="Settings" className="settings-icon" />
+                <div className="header-icons">
+                    <button className="new-log-btn">New Log</button>
+                    <button className="settings-icon">
+                        <i class="fa-solid fa-gear"></i>
+                    </button>
+                </div>
             </header>
+        </div>
+        <div className="dashboard-container">
             <main className="dashboard-main">
                 <h2 className="welcome-text">Welcome, User</h2>
-                <div className="button-group">
-                    <button className="btn log-product-btn">LOG NEW PRODUCT</button>
-                    <button className="btn saved-products-btn">SAVED PRODUCTS</button>
+                <div className="search-bar-container">
+                    <input type="text" placeholder="Search..." className="search-bar" />
+                    <button className="search-button">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
                 </div>
+                <div className="product-list">
+                    <button className="product-item">Almond Granola</button>
+                    <button className="product-item">Almond Granola</button>
+                    <button className="product-item">Almond Granola</button>
+                </div>
+                <button className="view-all-btn">View All</button>
             </main>
         </div>
+        </div>
+        
     );
 }
 
